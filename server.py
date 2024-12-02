1dcb1d8d32fe990d748bb35d1095c2d5227538b0
allenmylath <allengeorgemylath@gmail.com>
2024-12-02 21:58:46 +0530

    Update server.py

--- server.py
+++ server.py
@@ -33,4 +33,4 @@ async def websocket_endpoint(websocket: WebSocket):
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8765))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run("server:app", host="0.0.0.0", port=port, workers=workers)
