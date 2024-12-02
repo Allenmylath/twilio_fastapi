48ad19b1d1811d0a0ad670470655c7a602916a56
allenmylath <allengeorgemylath@gmail.com>
2024-12-02 21:45:01 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,29 +1,24 @@
 import json
-
+import os
 import uvicorn
-
 from fastapi import FastAPI, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse
-
 from bot import run_bot
 
 app = FastAPI()
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allow all origins for testing
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-
 @app.post("/")
 async def start_call():
     print("POST TwiML")
     return HTMLResponse(content=open("templates/streams.xml").read(), media_type="application/xml")
-
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
@@ -36,6 +31,6 @@ async def websocket_endpoint(websocket: WebSocket):
     print("WebSocket connection accepted")
     await run_bot(websocket, stream_sid)
 
-
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8765)
+    port = int(os.getenv("PORT", 8765))
+    uvicorn.run(app, host="0.0.0.0", port=port)
