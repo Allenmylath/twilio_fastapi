e78aca852e1ec2a3460443d981fa10beef81cca5
allenmylath <allengeorgemylath@gmail.com>
2025-01-22 13:48:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,7 +44,7 @@ async def run_bot(websocket_client, stream_sid):
         params=FastAPIWebsocketParams(
             audio_out_enabled=True,
             add_wav_header=False,
-            vad_enabled=True,
+            #vad_enabled=True,
             #vad_analyzer=SileroVADAnalyzer(),
             vad_audio_passthrough=True,
             serializer=TwilioFrameSerializer(stream_sid),
