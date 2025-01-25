20b73e486e048767b47c0c293b7c248ed1853954
allenmylath <allengeorgemylath@gmail.com>
2025-01-25 12:28:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,14 +8,20 @@ from pipecat.pipeline.runner import PipelineRunner
 from pipecat.pipeline.task import PipelineParams, PipelineTask
 from pipecat.processors.aggregators.openai_llm_context import OpenAILLMContext
 from pipecat.services.cartesia import CartesiaTTSService
-from pipecat.services.openai import OpenAILLMService
-from pipecat.services.deepgram import DeepgramSTTService
+
+# from pipecat.services.openai import OpenAILLMService
+from pipecat.services.groq import GroqLLMService
+
+# from pipecat.services.deepgram import DeepgramSTTService, GladiaSTTService
+from pipecat.services.gladia import GladiaSTTService
 from pipecat.transports.network.fastapi_websocket import (
     FastAPIWebsocketTransport,
     FastAPIWebsocketParams,
 )
 from pipecat.serializers.twilio import TwilioFrameSerializer
 from pipecat.audio.filters.noisereduce_filter import NoisereduceFilter
+
+from pdfreader import read_pdf
 
 from loguru import logger
 
@@ -41,19 +47,46 @@ async def run_bot(websocket_client, stream_sid):
         ),
     )
 
-    llm = OpenAILLMService(api_key=os.getenv("OPENAI_API_KEY"), model="gpt-4o")
+    # llm = OpenAILLMService(api_key=os.getenv("OPENAI_API_KEY"), model="gpt-4o")
+    llm = GroqLLMService(
+        api_key=os.getenv("GROQ_API_KEY"), model="llama3-groq-70b-8192-tool-use-preview"
+    )
 
-    stt = DeepgramSTTService(api_key=os.getenv("DEEPGRAM_API_KEY"))
+    # stt = DeepgramSTTService(api_key=os.getenv("DEEPGRAM_API_KEY"))
+    stt = GladiaSTTService(
+        api_key=os.getenv("GLADIA_API_KEY"),
+    )
 
     tts = CartesiaTTSService(
         api_key=os.getenv("CARTESIA_API_KEY"),
         voice_id="79a125e8-cd45-4c13-8a67-188112f4dd22",  # British Lady
     )
 
+    text = read_pdf("25 questions and responses for Jessica.pdf")
+
     messages = [
         {
             "role": "system",
-            "content": "You are a helpful assistant named Sarah at CARE ADHD Support Services. Your output will be converted to audio, so avoid using special characters in your answers. Dont give lond responses as user may get bored hearing long speech from converted audio.You should be warm and supportive while maintaining professional boundaries. You can assist with: General information about ADHD support programs, Basic service inquiries, Educational resource connections, Simple scheduling tasks. You must not provide medical advice or discuss personal health details. For any clinical questions or specific medical concerns, inform users that a qualified healthcare professional from the care team will contact them directly. Respond to users in a creative and helpful way, keeping your tone warm but professional. Focus on administrative and informational support only. When medical questions arise, gracefully transition to arranging contact with a human healthcare provider. Always remember your responses will be converted to audio, so maintain clear, natural speech patterns and AVOID TECHNICAL FORMATING AND SPECIAL CHARACTERS."
+            "content": (
+                "You are a helpful assistant named Jessica at CARE ADHD . "
+                "Your output will be converted to audio, so avoid using special characters in your answers. "
+                "Dont give lond responses as user may get bored hearing long speech from converted audio. "
+                "You should be warm and supportive while maintaining professional boundaries. "
+                "You can assist with: "
+                "General information about ADHD support programs, "
+                "Basic service inquiries, "
+                "Educational resource connections, "
+                "Simple scheduling tasks. "
+                "You must not provide medical advice or discuss personal health details. "
+                "For any clinical questions or specific medical concerns, inform users that a qualified "
+                "healthcare professional from the care team will contact them directly. "
+                "Respond to users in a creative and helpful way, keeping your tone warm but professional. "
+                "Focus on administrative and informational support only. "
+                "When medical questions arise, gracefully transition to arranging contact with a human healthcare provider. "
+                "When questions unrelated to ADHD or ADHD are asked gracefully transition to your core purpose"
+                "Always remember your responses will be converted to audio, so maintain clear, natural speech patterns "
+                "and AVOID TECHNICAL FORMATING AND SPECIAL CHARACTERS.\n\n" + text
+            ),
         }
     ]
 
@@ -77,7 +110,9 @@ async def run_bot(websocket_client, stream_sid):
     @transport.event_handler("on_client_connected")
     async def on_client_connected(transport, client):
         # Kick off the conversation.
-        messages.append({"role": "system", "content": "Please introduce yourself to the user."})
+        messages.append(
+            {"role": "system", "content": "Please introduce yourself to the user."}
+        )
         await task.queue_frames([LLMMessagesFrame(messages)])
 
     @transport.event_handler("on_client_disconnected")
