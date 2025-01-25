35c26d555546619814f3b13970be63af9f051ec0
allenmylath <allengeorgemylath@gmail.com>
2025-01-25 12:50:38 +0530

    Update server.py

--- server.py
+++ server.py
@@ -46,16 +46,12 @@ async def handle_incoming_call():
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    
-    # Get initial stream data
     start_data = websocket.iter_text()
     await start_data.__anext__()
     call_data = json.loads(await start_data.__anext__())
     print(call_data, flush=True)
-    
     stream_sid = call_data["start"]["streamSid"]
     print("WebSocket connection accepted")
-    
     await run_bot(websocket, stream_sid)
 
 if __name__ == "__main__":
