d31add6097d9bf67fb83199c4aba30ad9448d990
allenmylath <allengeorgemylath@gmail.com>
2025-02-20 14:01:06 +0530

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -241,6 +241,7 @@ async def run_bot(websocket_client, stream_sid, call_sid):
          api_key=os.getenv("CARTESIA_API_KEY"),
          voice_id="79a125e8-cd45-4c13-8a67-188112f4dd22",  # British Lady
          text_filter=MarkdownTextFilter(),
+         push_silence_after_stop=testing,
      )
      
     
