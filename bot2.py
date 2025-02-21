2fd28a283fd6ca4262d0810a7f5bcb58766c5c0d
allenmylath <allengeorgemylath@gmail.com>
2025-02-21 11:36:57 +0530

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -18,6 +18,7 @@ from pipecat.services.cartesia import CartesiaTTSService
 from pipecat.utils.text.markdown_text_filter import MarkdownTextFilter
 from pipecat.processors.aggregators.openai_llm_context import CustomEncoder
 from pipecat.processors.audio.vad.silero import SileroVAD
+from pipecat.processors.user_idle_processor import UserIdleProcessor
 from Audiobufferprocessor import AudioBufferProcessor
 from openai.types.chat import ChatCompletionToolParam
 from pipecat.processors.transcript_processor import TranscriptProcessor
