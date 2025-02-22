d2a0936f3d4c3f9910606449236f1a8ba9f6ccee
allenmylath <allengeorgemylath@gmail.com>
2025-02-22 19:24:43 +0530

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -221,7 +221,7 @@ async def run_bot(websocket_client, stream_sid, call_sid):
 
         stt = GladiaSTTService(
             api_key=os.getenv("GLADIA_API_KEY"),
-            audio_enhancer=False,
+            audio_enhancer=True,
             audio_passthrough=True,
         )
 
